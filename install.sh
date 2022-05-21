9e0eeeed23a4b411ef97e49862315e0aea392aa2
What? <98538512+sussygamedeveloper@users.noreply.github.com>
2022-05-21 13:33:29 -0400

    add install file

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd
+mkdir appl-and-orang
+cd appl-and-orang
+wget https://github.com/sussygamedeveloper/appl-and-orang-linux-files/raw/main/game.zip
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install unzip
+unzip game.zip
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+sudo apt install software-properties-common
+sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+sudo apt update
+sudo apt install --install-recommends winehq-stable 
+wine Appl\ and\ Orang\ 3d\ advatr.exe
