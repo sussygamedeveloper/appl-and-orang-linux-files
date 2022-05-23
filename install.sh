b080ea3b64c07d75d1b1bfaa45198252a79b1e59
What? <98538512+sussygamedeveloper@users.noreply.github.com>
2022-05-23 08:52:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,6 @@ sudo apt install software-properties-common
 sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 sudo apt update
 sudo apt install --install-recommends winehq-stable 
-wine Appl\ and\ Orang\ 3d\ advatr.exe
+wget https://github.com/sussygamedeveloper/appl-and-orang-linux-files/raw/main/launch.sh
+chmod +x launch.sh
+./launch.sh
